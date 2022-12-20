eb8d587c7ca21aaf73059719960b1e82f26b713f
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2022-12-20 15:20:39 +0900

    ログイン、ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_form_forgery with: :exceotion
+    include SessionsHelper
+    before_action :login_required
+
+    private
+
+    def login_required
+        redirect_to new_session_path unless current_user
+    end
 end
