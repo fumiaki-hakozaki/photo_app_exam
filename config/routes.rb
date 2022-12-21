a679db071503b48e2d8f9a77b0890e5e9f8f56c1
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2022-12-21 21:26:59 +0900

    プロフィール画像、編集実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create,:show]
+  resources :users do
+    collection do
+      post :account
+    end
+  end
 end
